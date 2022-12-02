d668ab16dadd54c44314b3242c6e4d90f53f0aac
rocha <rjoao0112@gmail.com>
2022-12-02 12:51:01 +0000

    tests

--- src/Recursive.java
+++ src/Recursive.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Recursive {
+public class Recursive {
+
+    public static int sumArray(int[] array, int arraySize){
+
+        if (arraySize == 0){
+            return array[arraySize];
+        }
+
+        return array[arraySize - 1] = array[arraySize] + sumArray(array, arraySize - 1);
+    }
+
+    public static int maxComumFactor(int num1, int num2){
+
+        if (num2 == 0){
+            return num1;
+        }
+
+        return maxComumFactor(num2, num1%num2);
+    }
+
+    public static int maxNumb(int[] array, int arraySize){
+        if (arraySize == 1){
+            return array[0];
+        }
+
+        if (array[0] < array[arraySize-1]){
+            array[0] = array[arraySize-1];
+            return maxNumb(array, arraySize - 1);
+        }
+
+        return maxNumb(array, arraySize - 1);
+    }
+
+
 }
